cd01cdbfe0ec313e95a8607013ad4a40528a3a65
damian <damianklepacki94@gmail.com>
2020-10-18 21:44:06 +0100

    add block hash search

--- js-backend/latestBlock.js
+++ js-backend/latestBlock.js
@@ -1,12 +1,19 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 async function main () {
+    const params = process.argv.slice(2)
     const wsProvider = new WsProvider('wss://rpc.polkadot.io');
     const api = await ApiPromise.create({ provider: wsProvider });
     const chain = await api.rpc.system.chain();
-    const lastHeader = await api.rpc.chain.getHeader();
-    console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`)
-    console.log(`Full head info: ${lastHeader}`)
+    if (params[0] === 'hash') {
+        const block = await api.rpc.chain.getBlock(params[1])
+        console.log(`Block number for hash: ${params[1]} is: ${block.block.header.number}, full block: ${block.block}`)
+      }else{
+        const lastHeader = await api.rpc.chain.getHeader();
+        console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`)
+        console.log(`Full head info: ${lastHeader}`)
+      }
+
 
 }
 
